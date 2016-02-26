dd38dfe123dc4e50fcd25dfd19e9ba082e20ddb1
Gangrin <gangrin@gmail.com>
2016-02-26 18:51:36 +0100

    My first calc in lua.

---
+++ calc.lua
@@ -0,0 +1,47 @@
+function calc(a)
+  sum = string.find(a, "+")
+  sub = string.find(a, "-")
+  multi = string.find(a, "*")
+  divi = string.find(a, "/")
+
+  res = 0
+
+  if type(sum) == "number" and sum > 1 then
+    res = sum
+  elseif type(sub) == "number" and sub > 1 then
+    res = sub
+  elseif type(multi) == "number" and multi > 1 then
+    res = multi
+  elseif type(divi) == "number" and divi > 1 then
+    res = divi
+  else
+    print("No way, fucker!")
+  end
+
+  w = (res-1)
+  e = (res+1)
+
+  x = string.sub(a, 1, w)
+  y = string.sub(a, e)
+
+  all = 0
+
+  if type(sum) == "number" and sum > 1 then
+    all = (x+y)
+  elseif type(sub) == "number" and sub > 1 then
+    all = (x-y)
+  elseif type(multi) == "number" and multi > 1 then
+    all = (x*y)
+  elseif type(divi) == "number" and divi > 1 then
+    all = (x/y)
+  else
+    print("No way, fucker!")
+  end
+
+  print(all)
+end
+
+
+print ("Give:")
+a = io.read("*line")
+print (calc(a))
